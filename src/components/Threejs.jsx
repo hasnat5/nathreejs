5f8bdc509f02a4b783f46ddc37edfb481bbad336
hasnat5 <hasnatferdiananda5@gmail.com>
2023-06-02 14:52:21 +0700

    first commit

---
+++ src/components/Threejs.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const Threejs = () => {
+    return (
+        <div className='relative'>
+            <Spline scene="https://prod.spline.design/skbnx1mR3v8ezheo/scene.splinecode" />
+        </div>
+
+
+    )
+}
+
+export default Threejs
