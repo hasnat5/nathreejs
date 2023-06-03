cbf9e1a24d976e6e2d7d78c109ec377ace95a7aa
hasnat5 <hasnatferdiananda5@gmail.com>
2023-06-03 13:04:23 +0700

    fix text

--- src/app/natpanobig/page.jsx
+++ src/app/natpanobig/page.jsx
@@ -5,7 +5,7 @@ const page = () => {
     return (
         <section className='container min-w-full overflow-hidden h-screen relative'>
 
-            <h2 className='text-center text-5xl font-bold absolute left-0 right-0 mx-auto'>panoramic small</h2>
+            <h2 className='text-center text-5xl font-bold absolute left-0 right-0 mx-auto'>panoramic big</h2>
             <PanoBig />
         </section>
     )
