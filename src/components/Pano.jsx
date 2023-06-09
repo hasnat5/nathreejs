feb13b888253608338ab2b4a56b5f102d3c14929
hasnat5 <hasnatferdiananda5@gmail.com>
2023-06-09 10:54:13 +0700

    add another image

--- src/components/Pano.jsx
+++ src/components/Pano.jsx
@@ -12,10 +12,31 @@ const Pano = () => {
         if (isClient) {
             const PANOLENS = require('panolens'); // Use require for server-side compatibility
 
-            const viewer = new PANOLENS.Viewer({ container: document.getElementById('panoramic-small') });
+            const viewer = new PANOLENS.Viewer({ container: document.getElementById('panoramic-small'), enableReticle: false, output: 'console', viewIndicator: true });
 
             const panorama = new PANOLENS.ImagePanorama('assets/image/Panoramic-2-small.png');
-            viewer.add(panorama);
+            const castle = new PANOLENS.ImagePanorama('assets/image/natpano.jpg');
+
+            const infospot = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
+            infospot.position.set(-100, -500, 5000);
+            infospot.addHoverText("Gedung");
+
+            infospot.addEventListener('click', function () {
+                viewer.setPanorama(castle);
+            });
+
+            const infospot2 = new PANOLENS.Infospot(500, PANOLENS.DataImage.Info);
+            infospot2.position.set(-100, -500, 5000);
+            infospot2.addHoverText("Nikahan");
+
+            infospot2.addEventListener('click', function () {
+                viewer.setPanorama(panorama);
+            });
+
+            panorama.add(infospot);
+            castle.add(infospot2);
+
+            viewer.add(panorama, castle);
         }
     }, [isClient]);
 
